edd3b293ee514f09c1bf75f206568b6a501aaa29
freerangeandy <freerangeandy@gmail.com>
2020-08-21 09:06:19 -0400

    added darkened mountain background to gameover screen

--- src/scenes/GameOverScreen.js
+++ src/scenes/GameOverScreen.js
@@ -1,5 +1,9 @@
 import Phaser from "phaser";
-
+import mountainBG from "../assets/parallax-mountain-bg.png"
+import mountainFar from "../assets/parallax-mountain-montain-far.png"
+import mountains from "../assets/parallax-mountain-mountains.png"
+import mountainTrees from "../assets/parallax-mountain-trees.png"
+import mountainFGTrees from "../assets/parallax-mountain-foreground-trees.png"
 import flippieStand from "../assets/flyingbird.png"
 import { gameOptions } from "../constants"
 
@@ -12,13 +16,20 @@ export default new Phaser.Class({
     Phaser.Scene.call(this, { key: 'gameoverscreen' });
   },
   preload: function() {
+    this.load.image('mountainBG', mountainBG);
+    this.load.image('mountainFar', mountainFar);
+    this.load.image('mountains', mountains);
+    this.load.image('mountainTrees', mountainTrees);
+    this.load.image('mountainFGTrees', mountainFGTrees);
+
     this.load.spritesheet('flippieStand', flippieStand, { frameWidth: 32, frameHeight: 32, endFrame: 6 })
   },
   create: function() {
     cursors = this.input.keyboard.createCursorKeys();
 
+    this.addBackground()
     graphics = this.add.graphics();
-    graphics.fillStyle(0x000000, 1);
+    graphics.fillStyle(0x000000, 0.7);
     graphics.fillRect(0, 0, gameOptions.gameWidth, gameOptions.gameHeight);
 
     this.loadStandingFlippie()
@@ -30,6 +41,57 @@ export default new Phaser.Class({
     if (cursors.space.isDown) {
       this.scene.start('splashscreen');
     }
+  },
+  addBackground: function(){
+    this.mountainsBack = this.add.tileSprite(
+      gameOptions.gameWidth/2,
+      gameOptions.gameHeight/2,
+      gameOptions.gameWidth,
+      gameOptions.gameHeight,
+      'mountainBG'
+    )
+    this.mountainsBack.tileScaleX=2
+    this.mountainsBack.tileScaleY=2
+
+    this.mountainsMid3 = this.add.tileSprite(
+      gameOptions.gameWidth/2,
+      gameOptions.gameHeight/2,
+      gameOptions.gameWidth,
+      gameOptions.gameHeight,
+      'mountainFar'
+    )
+    this.mountainsMid3.tileScaleX=2
+    this.mountainsMid3.tileScaleY=2
+
+    this.mountainsMid2 = this.add.tileSprite(
+      gameOptions.gameWidth/2,
+      gameOptions.gameHeight/2,
+      gameOptions.gameWidth,
+      gameOptions.gameHeight,
+      'mountains'
+    )
+    this.mountainsMid2.tileScaleX=2
+    this.mountainsMid2.tileScaleY=2
+
+    this.mountainsMid1 = this.add.tileSprite(
+      gameOptions.gameWidth/2,
+      gameOptions.gameHeight/2,
+      gameOptions.gameWidth,
+      gameOptions.gameHeight,
+      'mountainTrees'
+    )
+    this.mountainsMid1.tileScaleX=2
+    this.mountainsMid1.tileScaleY=2
+
+    this.mountainsFront = this.add.tileSprite(
+      gameOptions.gameWidth/2,
+      gameOptions.gameHeight/2,
+      gameOptions.gameWidth,
+      gameOptions.gameHeight,
+      'mountainFGTrees'
+    )
+    this.mountainsFront.tileScaleX=2
+    this.mountainsFront.tileScaleY=2
   },
   loadStandingFlippie: function(){
     this.anims.create({
